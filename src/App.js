8961fcf54e5b7eb8650ad7842ea50fcfe708f3a5
Jen Speak <jspeak6@gmail.com>
2021-04-26 16:01:56 -0700

    add Spacer

--- src/App.js
+++ src/App.js
@@ -5,43 +5,46 @@ import GlobalStyles from './components/GlobalStyles';
 import Logo from './components/Logo';
 import Controls from './components/Controls';
 import Timer from './components/Timer';
+import Spacer from './components/Spacer';
 
 function App() {
 	const [pomoLength, setPomoLength] = useState(25);
-  const [shortLength, setShortLength] = useState(5);
-  const [longLength, setLongLength] = useState(15);
-  const [isTimerOn, setIsTimerOn] = useState(false);
-  const [secondsLeft, setSecondsLeft] = useState(pomoLength * 60);
-  const [timerMode, setTimerMode] = useState('pomodoro');
+	const [shortLength, setShortLength] = useState(5);
+	const [longLength, setLongLength] = useState(15);
+	const [isTimerOn, setIsTimerOn] = useState(false);
+	const [secondsLeft, setSecondsLeft] = useState(pomoLength * 60);
+	const [timerMode, setTimerMode] = useState('pomodoro');
 
 	return (
 		<Wrapper>
-      <GlobalStyles />
-      <Logo />
+			<GlobalStyles />
+			<Logo />
+			<Spacer size={45} mobileLargeAndUp={55}/>
 			<Controls
-        setSecondsLeft={setSecondsLeft}
-        timerMode={timerMode}
-        setTimerMode={setTimerMode}
-        pomoLength={pomoLength}
-        shortLength={shortLength}
-        longLength={longLength}
-        setIsTimerOn={setIsTimerOn}
+				setSecondsLeft={setSecondsLeft}
+				timerMode={timerMode}
+				setTimerMode={setTimerMode}
+				pomoLength={pomoLength}
+				shortLength={shortLength}
+				longLength={longLength}
+				setIsTimerOn={setIsTimerOn}
 			/>
+      <Spacer size={48} mobileLargeAndUp={109} desktopAndUp={45}/>
 			<Timer
 				pomoLength={pomoLength}
 				secondsLeft={secondsLeft}
 				setSecondsLeft={setSecondsLeft}
-        isTimerOn={isTimerOn}
-        setIsTimerOn={setIsTimerOn}
+				isTimerOn={isTimerOn}
+				setIsTimerOn={setIsTimerOn}
 			/>
 		</Wrapper>
 	);
 }
 
 const Wrapper = styled.main`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
 `;
 
 export default App;
---
+++ src/components/Spacer.js
@@ -0,0 +1,32 @@
+// Borrowed and tweaked from https://www.joshwcomeau.com/react/modern-spacer-gif/
+// Used to create space between components
+import styled from 'styled-components/macro';
+
+function getResponsiveSpace(breakpoint) {
+	return typeof breakpoint === 'number' ? breakpoint + 'px' : undefined;
+}
+
+const Spacer = styled.span`
+	display: block;
+	width: ${p => p.size}px;
+	min-width: ${p => p.size}px;
+	height: ${p => p.size}px;
+	min-height: ${p => p.size}px;
+
+	// Make Spacer responsive
+	@media (min-width: 600px) {
+		width: ${p => getResponsiveSpace(p.mobileLargeAndUp)};
+		min-width: ${p => getResponsiveSpace(p.mobileLargeAndUp)};
+		height: ${p => getResponsiveSpace(p.mobileLargeAndUp)};
+		min-height: ${p => getResponsiveSpace(p.mobileLargeAndUp)};
+	}
+
+	@media (min-width: 1024px) {
+		width: ${p => getResponsiveSpace(p.desktopAndUp)};
+		min-width: ${p => getResponsiveSpace(p.desktopAndUp)};
+		height: ${p => getResponsiveSpace(p.desktopAndUp)};
+		min-height: ${p => getResponsiveSpace(p.desktopAndUp)};
+	}
+`;
+
+export default Spacer;
